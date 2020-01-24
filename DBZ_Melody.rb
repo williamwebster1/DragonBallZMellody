37bb7086d521e9575cfdea80613ff0104586dcd9
williamwebster1 <37878196+williamwebster1@users.noreply.github.com>
2020-01-24 14:47:39 -0800

    Add files via upload

---
+++ DBZ_Melody.rb
@@ -0,0 +1,117 @@
+use_bpm 162
+play :G5
+play :G4
+sleep 1
+play :A5
+play :A4
+sleep 1
+play :G5
+play :G4
+sleep 1
+play :A5
+play :A4
+sleep 1
+
+play :G5
+play :G4
+sleep 1
+play :F4
+play :F5
+sleep 0.5
+play :E4
+play :E5
+sleep 1
+play :D4
+play :D5
+sleep 1.2
+
+
+
+play :F4
+play :F5
+sleep 1
+play :E4
+play :E5
+sleep 1
+play :F4
+play :F5
+sleep 1
+
+
+
+play :E4
+play :E5
+sleep 1
+play :D4
+play :D5
+sleep 1
+play :C4
+play :C5
+sleep 1
+play :B3
+play :B4
+
+
+sleep 1
+play :A3
+play :A4
+sleep 1
+play :C4
+play :C5
+sleep 1
+play :F4
+play :F5
+
+
+
+sleep 1
+play :E4
+play :E5
+sleep 1
+play :G4
+play :G5
+sleep 0.5
+play :C4
+play :C5
+
+
+
+
+sleep 1
+play :D4
+play :D5
+sleep 0.7
+play :E4
+play :E5
+sleep 0.7
+play :F4
+play :F5
+sleep 0.7
+play :D4
+play :D5
+sleep 0.7
+play :E4
+play :E5
+sleep 0.7
+play :F4
+play :F5
+
+
+sleep 1
+play :G4
+play :B4
+play :D5
+play :G5
+sleep 1
+play :G4
+play :B4
+play :D5
+play :G5
+sleep 1
+play :A4
+play :A5
+sleep 1
+play :B3
+play :B4
+sleep 0.5
+play :C4
